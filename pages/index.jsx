c422207ec4e1de4f426437b25d66801b60b7f59a
Hanan Asyrawi <hasyrawi@gmail.com>
2020-11-15 05:58:00 +0800

    more css

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,6 +1,8 @@
-import Head from 'next/head'
-import Layout from '../layouts/Layout'
+import Head from "next/head";
+import Layout from "../layouts/Layout";
 export default function Home({ response }) {
+  const { data } = response;
+
   return (
     <>
       <Head>
@@ -8,91 +10,16 @@ export default function Home({ response }) {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <Layout>
-        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Laborum laboriosam, ad sint amet perspiciatis nihil voluptatibus saepe, sequi natus debitis quis dolore pariatur, nobis at maxime aspernatur. Accusantium, impedit dolorem!
-        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Laborum laboriosam, ad sint amet perspiciatis nihil voluptatibus saepe, sequi natus debitis quis dolore pariatur, nobis at maxime aspernatur. Accusantium, impedit dolorem!
-        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Laborum laboriosam, ad sint amet perspiciatis nihil voluptatibus saepe, sequi natus debitis quis dolore pariatur, nobis at maxime aspernatur. Accusantium, impedit dolorem!
-        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Laborum laboriosam, ad sint amet perspiciatis nihil voluptatibus saepe, sequi natus debitis quis dolore pariatur, nobis at maxime aspernatur. Accusantium, impedit dolorem!
-        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Laborum laboriosam, ad sint amet perspiciatis nihil voluptatibus saepe, sequi natus debitis quis dolore pariatur, nobis at maxime aspernatur. Accusantium, impedit dolorem!
-        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Laborum laboriosam, ad sint amet perspiciatis nihil voluptatibus saepe, sequi natus debitis quis dolore pariatur, nobis at maxime aspernatur. Accusantium, impedit dolorem!
-        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Laborum laboriosam, ad sint amet perspiciatis nihil voluptatibus saepe, sequi natus debitis quis dolore pariatur, nobis at maxime aspernatur. Accusantium, impedit dolorem!
-        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Laborum laboriosam, ad sint amet perspiciatis nihil voluptatibus saepe, sequi natus debitis quis dolore pariatur, nobis at maxime aspernatur. Accusantium, impedit dolorem!
-        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Laborum laboriosam, ad sint amet perspiciatis nihil voluptatibus saepe, sequi natus debitis quis dolore pariatur, nobis at maxime aspernatur. Accusantium, impedit dolorem!
-        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Laborum laboriosam, ad sint amet perspiciatis nihil voluptatibus saepe, sequi natus debitis quis dolore pariatur, nobis at maxime aspernatur. Accusantium, impedit dolorem!
-        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Laborum laboriosam, ad sint amet perspiciatis nihil voluptatibus saepe, sequi natus debitis quis dolore pariatur, nobis at maxime aspernatur. Accusantium, impedit dolorem!
-        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Laborum laboriosam, ad sint amet perspiciatis nihil voluptatibus saepe, sequi natus debitis quis dolore pariatur, nobis at maxime aspernatur. Accusantium, impedit dolorem!
-        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Laborum laboriosam, ad sint amet perspiciatis nihil voluptatibus saepe, sequi natus debitis quis dolore pariatur, nobis at maxime aspernatur. Accusantium, impedit dolorem!
-        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Laborum laboriosam, ad sint amet perspiciatis nihil voluptatibus saepe, sequi natus debitis quis dolore pariatur, nobis at maxime aspernatur. Accusantium, impedit dolorem!
-        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Laborum laboriosam, ad sint amet perspiciatis nihil voluptatibus saepe, sequi natus debitis quis dolore pariatur, nobis at maxime aspernatur. Accusantium, impedit dolorem!
-        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Laborum laboriosam, ad sint amet perspiciatis nihil voluptatibus saepe, sequi natus debitis quis dolore pariatur, nobis at maxime aspernatur. Accusantium, impedit dolorem!
-        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Laborum laboriosam, ad sint amet perspiciatis nihil voluptatibus saepe, sequi natus debitis quis dolore pariatur, nobis at maxime aspernatur. Accusantium, impedit dolorem!
-        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Laborum laboriosam, ad sint amet perspiciatis nihil voluptatibus saepe, sequi natus debitis quis dolore pariatur, nobis at maxime aspernatur. Accusantium, impedit dolorem!
-        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Laborum laboriosam, ad sint amet perspiciatis nihil voluptatibus saepe, sequi natus debitis quis dolore pariatur, nobis at maxime aspernatur. Accusantium, impedit dolorem!
-        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Laborum laboriosam, ad sint amet perspiciatis nihil voluptatibus saepe, sequi natus debitis quis dolore pariatur, nobis at maxime aspernatur. Accusantium, impedit dolorem!
-        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Laborum laboriosam, ad sint amet perspiciatis nihil voluptatibus saepe, sequi natus debitis quis dolore pariatur, nobis at maxime aspernatur. Accusantium, impedit dolorem!
-        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Laborum laboriosam, ad sint amet perspiciatis nihil voluptatibus saepe, sequi natus debitis quis dolore pariatur, nobis at maxime aspernatur. Accusantium, impedit dolorem!
-        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Laborum laboriosam, ad sint amet perspiciatis nihil voluptatibus saepe, sequi natus debitis quis dolore pariatur, nobis at maxime aspernatur. Accusantium, impedit dolorem!
-        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Laborum laboriosam, ad sint amet perspiciatis nihil voluptatibus saepe, sequi natus debitis quis dolore pariatur, nobis at maxime aspernatur. Accusantium, impedit dolorem!
-        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Laborum laboriosam, ad sint amet perspiciatis nihil voluptatibus saepe, sequi natus debitis quis dolore pariatur, nobis at maxime aspernatur. Accusantium, impedit dolorem!
-        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Laborum laboriosam, ad sint amet perspiciatis nihil voluptatibus saepe, sequi natus debitis quis dolore pariatur, nobis at maxime aspernatur. Accusantium, impedit dolorem!
-        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Laborum laboriosam, ad sint amet perspiciatis nihil voluptatibus saepe, sequi natus debitis quis dolore pariatur, nobis at maxime aspernatur. Accusantium, impedit dolorem!
-        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Laborum laboriosam, ad sint amet perspiciatis nihil voluptatibus saepe, sequi natus debitis quis dolore pariatur, nobis at maxime aspernatur. Accusantium, impedit dolorem!
-        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Laborum laboriosam, ad sint amet perspiciatis nihil voluptatibus saepe, sequi natus debitis quis dolore pariatur, nobis at maxime aspernatur. Accusantium, impedit dolorem!
-        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Laborum laboriosam, ad sint amet perspiciatis nihil voluptatibus saepe, sequi natus debitis quis dolore pariatur, nobis at maxime aspernatur. Accusantium, impedit dolorem!
-        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Laborum laboriosam, ad sint amet perspiciatis nihil voluptatibus saepe, sequi natus debitis quis dolore pariatur, nobis at maxime aspernatur. Accusantium, impedit dolorem!
-        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Laborum laboriosam, ad sint amet perspiciatis nihil voluptatibus saepe, sequi natus debitis quis dolore pariatur, nobis at maxime aspernatur. Accusantium, impedit dolorem!
-        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Laborum laboriosam, ad sint amet perspiciatis nihil voluptatibus saepe, sequi natus debitis quis dolore pariatur, nobis at maxime aspernatur. Accusantium, impedit dolorem!
-        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Laborum laboriosam, ad sint amet perspiciatis nihil voluptatibus saepe, sequi natus debitis quis dolore pariatur, nobis at maxime aspernatur. Accusantium, impedit dolorem!
-        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Laborum laboriosam, ad sint amet perspiciatis nihil voluptatibus saepe, sequi natus debitis quis dolore pariatur, nobis at maxime aspernatur. Accusantium, impedit dolorem!
-        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Laborum laboriosam, ad sint amet perspiciatis nihil voluptatibus saepe, sequi natus debitis quis dolore pariatur, nobis at maxime aspernatur. Accusantium, impedit dolorem!
-        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Laborum laboriosam, ad sint amet perspiciatis nihil voluptatibus saepe, sequi natus debitis quis dolore pariatur, nobis at maxime aspernatur. Accusantium, impedit dolorem!
-        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Laborum laboriosam, ad sint amet perspiciatis nihil voluptatibus saepe, sequi natus debitis quis dolore pariatur, nobis at maxime aspernatur. Accusantium, impedit dolorem!
-        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Laborum laboriosam, ad sint amet perspiciatis nihil voluptatibus saepe, sequi natus debitis quis dolore pariatur, nobis at maxime aspernatur. Accusantium, impedit dolorem!
-        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Laborum laboriosam, ad sint amet perspiciatis nihil voluptatibus saepe, sequi natus debitis quis dolore pariatur, nobis at maxime aspernatur. Accusantium, impedit dolorem!
-        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Laborum laboriosam, ad sint amet perspiciatis nihil voluptatibus saepe, sequi natus debitis quis dolore pariatur, nobis at maxime aspernatur. Accusantium, impedit dolorem!
-        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Laborum laboriosam, ad sint amet perspiciatis nihil voluptatibus saepe, sequi natus debitis quis dolore pariatur, nobis at maxime aspernatur. Accusantium, impedit dolorem!
-        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Laborum laboriosam, ad sint amet perspiciatis nihil voluptatibus saepe, sequi natus debitis quis dolore pariatur, nobis at maxime aspernatur. Accusantium, impedit dolorem!
-        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Laborum laboriosam, ad sint amet perspiciatis nihil voluptatibus saepe, sequi natus debitis quis dolore pariatur, nobis at maxime aspernatur. Accusantium, impedit dolorem!
-        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Laborum laboriosam, ad sint amet perspiciatis nihil voluptatibus saepe, sequi natus debitis quis dolore pariatur, nobis at maxime aspernatur. Accusantium, impedit dolorem!
-        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Laborum laboriosam, ad sint amet perspiciatis nihil voluptatibus saepe, sequi natus debitis quis dolore pariatur, nobis at maxime aspernatur. Accusantium, impedit dolorem!
-        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Laborum laboriosam, ad sint amet perspiciatis nihil voluptatibus saepe, sequi natus debitis quis dolore pariatur, nobis at maxime aspernatur. Accusantium, impedit dolorem!
-        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Laborum laboriosam, ad sint amet perspiciatis nihil voluptatibus saepe, sequi natus debitis quis dolore pariatur, nobis at maxime aspernatur. Accusantium, impedit dolorem!
-        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Laborum laboriosam, ad sint amet perspiciatis nihil voluptatibus saepe, sequi natus debitis quis dolore pariatur, nobis at maxime aspernatur. Accusantium, impedit dolorem!
-        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Laborum laboriosam, ad sint amet perspiciatis nihil voluptatibus saepe, sequi natus debitis quis dolore pariatur, nobis at maxime aspernatur. Accusantium, impedit dolorem!
-        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Laborum laboriosam, ad sint amet perspiciatis nihil voluptatibus saepe, sequi natus debitis quis dolore pariatur, nobis at maxime aspernatur. Accusantium, impedit dolorem!
-        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Laborum laboriosam, ad sint amet perspiciatis nihil voluptatibus saepe, sequi natus debitis quis dolore pariatur, nobis at maxime aspernatur. Accusantium, impedit dolorem!
-        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Laborum laboriosam, ad sint amet perspiciatis nihil voluptatibus saepe, sequi natus debitis quis dolore pariatur, nobis at maxime aspernatur. Accusantium, impedit dolorem!
-        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Laborum laboriosam, ad sint amet perspiciatis nihil voluptatibus saepe, sequi natus debitis quis dolore pariatur, nobis at maxime aspernatur. Accusantium, impedit dolorem!
-        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Laborum laboriosam, ad sint amet perspiciatis nihil voluptatibus saepe, sequi natus debitis quis dolore pariatur, nobis at maxime aspernatur. Accusantium, impedit dolorem!
-        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Laborum laboriosam, ad sint amet perspiciatis nihil voluptatibus saepe, sequi natus debitis quis dolore pariatur, nobis at maxime aspernatur. Accusantium, impedit dolorem!
-        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Laborum laboriosam, ad sint amet perspiciatis nihil voluptatibus saepe, sequi natus debitis quis dolore pariatur, nobis at maxime aspernatur. Accusantium, impedit dolorem!
-        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Laborum laboriosam, ad sint amet perspiciatis nihil voluptatibus saepe, sequi natus debitis quis dolore pariatur, nobis at maxime aspernatur. Accusantium, impedit dolorem!
-        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Laborum laboriosam, ad sint amet perspiciatis nihil voluptatibus saepe, sequi natus debitis quis dolore pariatur, nobis at maxime aspernatur. Accusantium, impedit dolorem!
-        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Laborum laboriosam, ad sint amet perspiciatis nihil voluptatibus saepe, sequi natus debitis quis dolore pariatur, nobis at maxime aspernatur. Accusantium, impedit dolorem!
-        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Laborum laboriosam, ad sint amet perspiciatis nihil voluptatibus saepe, sequi natus debitis quis dolore pariatur, nobis at maxime aspernatur. Accusantium, impedit dolorem!
-        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Laborum laboriosam, ad sint amet perspiciatis nihil voluptatibus saepe, sequi natus debitis quis dolore pariatur, nobis at maxime aspernatur. Accusantium, impedit dolorem!
-        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Laborum laboriosam, ad sint amet perspiciatis nihil voluptatibus saepe, sequi natus debitis quis dolore pariatur, nobis at maxime aspernatur. Accusantium, impedit dolorem!
-        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Laborum laboriosam, ad sint amet perspiciatis nihil voluptatibus saepe, sequi natus debitis quis dolore pariatur, nobis at maxime aspernatur. Accusantium, impedit dolorem!
-        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Laborum laboriosam, ad sint amet perspiciatis nihil voluptatibus saepe, sequi natus debitis quis dolore pariatur, nobis at maxime aspernatur. Accusantium, impedit dolorem!
-        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Laborum laboriosam, ad sint amet perspiciatis nihil voluptatibus saepe, sequi natus debitis quis dolore pariatur, nobis at maxime aspernatur. Accusantium, impedit dolorem!
-        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Laborum laboriosam, ad sint amet perspiciatis nihil voluptatibus saepe, sequi natus debitis quis dolore pariatur, nobis at maxime aspernatur. Accusantium, impedit dolorem!
-        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Laborum laboriosam, ad sint amet perspiciatis nihil voluptatibus saepe, sequi natus debitis quis dolore pariatur, nobis at maxime aspernatur. Accusantium, impedit dolorem!
-        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Laborum laboriosam, ad sint amet perspiciatis nihil voluptatibus saepe, sequi natus debitis quis dolore pariatur, nobis at maxime aspernatur. Accusantium, impedit dolorem!
-        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Laborum laboriosam, ad sint amet perspiciatis nihil voluptatibus saepe, sequi natus debitis quis dolore pariatur, nobis at maxime aspernatur. Accusantium, impedit dolorem!
-        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Laborum laboriosam, ad sint amet perspiciatis nihil voluptatibus saepe, sequi natus debitis quis dolore pariatur, nobis at maxime aspernatur. Accusantium, impedit dolorem!
-        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Laborum laboriosam, ad sint amet perspiciatis nihil voluptatibus saepe, sequi natus debitis quis dolore pariatur, nobis at maxime aspernatur. Accusantium, impedit dolorem!
-        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Laborum laboriosam, ad sint amet perspiciatis nihil voluptatibus saepe, sequi natus debitis quis dolore pariatur, nobis at maxime aspernatur. Accusantium, impedit dolorem!
-        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Laborum laboriosam, ad sint amet perspiciatis nihil voluptatibus saepe, sequi natus debitis quis dolore pariatur, nobis at maxime aspernatur. Accusantium, impedit dolorem!
-        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Laborum laboriosam, ad sint amet perspiciatis nihil voluptatibus saepe, sequi natus debitis quis dolore pariatur, nobis at maxime aspernatur. Accusantium, impedit dolorem!
+        
       </Layout>
     </>
-  )
+  );
 }
 
 export async function getServerSideProps(ctx) {
-  const request = await fetch("http://localhost:3000/api/category/")
-  const response = await request.json()
+  const request = await fetch("http://localhost:3000/api/category/");
+  const response = await request.json();
   return {
-    props: { response }
-  }
+    props: { response },
+  };
 }
-

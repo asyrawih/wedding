32997e51d682e4a3a2ccd6693d32995784061ceb
Hanan Asyrawi <hasyrawi@gmail.com>
2020-11-15 08:46:45 +0800

    remove purge

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,12 +3,7 @@ module.exports = {
     removeDeprecatedGapUtilities: true,
     purgeLayersByDefault: true,
   },
-  purge: 
-     [
-      './src/**/*.html',
-      './src/**/*.vue',
-      './src/**/*.jsx'
-    ],
+  purge: [],
   target: 'relaxed',
   prefix: '',
   important: false,
